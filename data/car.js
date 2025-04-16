066030d43da1e4054c5369961bfb16284c6920e4
AinurMac <ainurbogdanov69@gmail.com>
2025-04-16 06:42:30 +0400

    17d exsersise prep

--- data/car.js
+++ data/car.js
@@ -1,22 +1,52 @@
 class car {
   brand;
   model;
+  speed;
+  isTrunkOpen;
 
   constructor(carDetails) {
     this.brand = carDetails.brand,
-    this.model = carDetails.model
+    this.model = carDetails.model,
+    this.speed = carDetails.speed,
+    this.isTrunkOpen = carDetails.speed
+
+  };
+
+  displayInfo() {
+    console.log(`brand is: ${this.brand}, model is ${this.model}, speed is: ${this.speed}`)
+  };
+
+  go() {
+   if(this.speed < 196) this.speed += 5 
+    
+  };
+
+  brake() {
+   if(this.speed >= 5) this.speed -= 5
+  };
+
+  openTrank() {
+
   }
-};
+  closeTrank() {
+
+  }
+}
 
 const car1 = new car({
   brand: 'Toyota',
-  model: 'Corolla' 
+  model: 'Corolla',
+  speed: 0,
+  isTrunkOpen: false
 });
 
 const car2 = new car({
   brand: 'Tesla',
-  model: 'Model 3'
+  model: 'Model 3',
+  speed: 0,
+  isTrunkOpen: false
 });
 
-console.log(car1)
-console.log(car2)
+
+car1.displayInfo();
+car2.displayInfo();
