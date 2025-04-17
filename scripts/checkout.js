8d315401096eb2e4370124244588f61c918b603e
AinurMac <ainurbogdanov69@gmail.com>
2025-04-17 07:14:28 +0400

    Learn promoses and conver code into promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,12 +2,55 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import renderCheckoutHeader from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/car.js";
 // import "../data/backend-practice.js";
 // import '../data/cart-class.js';
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }), 
+  new Promise ((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values)
   renderOrderSummary();
   renderCheckoutHeader();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value)
+  return new Promise ((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderCheckoutHeader();
+  renderPaymentSummary();
+});
+*/
+
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderCheckoutHeader();
+//     renderPaymentSummary();
+//   });
+// });
